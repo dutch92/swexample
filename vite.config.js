ee6d4d18168d03cef0b4080bfae8c29ee78b55bc
Alex Smirnov <alex.smirnov@jetrockets.com>
2023-12-14 18:50:20 +0300

    h

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import {VitePWA} from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    filename: 'my-sw.js',
-    workbox: {
+    strategies: 'injectManifest',
+    injectManifest: {
       globPatterns: ['**/*.{js,css,html,ico,png,svg,otf}']
-    }
+    },
+    filename: 'my-sw.js',
   })],
   base: '/swexample/',
 })
